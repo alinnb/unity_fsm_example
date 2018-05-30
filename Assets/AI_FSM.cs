7b8dd702b20491e074f5b092d683214d5b95ff4f
alinnb <alvinzheng@gmail.com>
2018-05-30 19:25:46 +0800

    fsm example for unity

---
+++ Assets/AI_FSM.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+using Spine.Unity;
+
+public class AI_FSM : MonoBehaviour {
+	
+	public GameObject hero;
+	public GameObject patrolA;
+	public GameObject patrolB;
+	public GameObject birthPoint;
+
+	public float range = 10;
+	public float attackRange = 1;
+
+	enum State {
+		State_idle = 0,
+		State_follow = 1,
+		State_attack = 2,
+		State_back = 4,
+		State_patrol = 5,
+	};
+
+	enum IdleState {
+		IdleState_idle1 = 0,
+		IdleState_idle2 = 1,
+		IdleState_idle3 = 2,
+	};
+
+	string idleAnimationName = "Idle";
+	string idleAnimationName2 = "Idle2";
+	string idleAnimationName3 = "Idle3";
+	string moveAnimation = "Move";
+	string attackAnimationName = "Attack";
+
+	float speed = 2.0f;
+
+	State state;
+	IdleState idleState;
+
+	SkeletonAnimation skeletonAnimation;
+	string curAnimationName;
+
+	Spine.AnimationState spineAnimationState;
+
+	float cutTime;
+
+	// Use this for initialization
+	void Start () {
+		state = State.State_idle;
+		idleState = IdleState.IdleState_idle1;
+		cutTime = Time.time;
+		skeletonAnimation = GetComponent<SkeletonAnimation>();
+
+		curAnimationName = "";
+		spineAnimationState = skeletonAnimation.state;
+		playAnimation (idleAnimationName);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		switch (state) {
+		case State.State_idle:
+			float newTime = Time.time;
+			switch (idleState) {
+			case IdleState.IdleState_idle1:
+				playAnimation (idleAnimationName);
+				if (Time.time - cutTime > 4) {
+					idleState = IdleState.IdleState_idle2;
+					cutTime = newTime;
+				}
+				break;
+			case IdleState.IdleState_idle2:
+				playAnimation (idleAnimationName2);
+				if (Time.time - cutTime > 4) {
+					idleState = IdleState.IdleState_idle3;
+					cutTime = newTime;
+				}
+				break;
+			case IdleState.IdleState_idle3:
+				playAnimation (idleAnimationName3);
+				if (Time.time - cutTime > 4) {
+					idleState = IdleState.IdleState_idle1;
+					cutTime = newTime;
+				}
+				break;
+			}
+			if (getDistance (this.gameObject, hero) < range) {
+				setState (State.State_follow);
+			}
+			break;
+		case State.State_follow:
+			playAnimation (moveAnimation);
+			moveTo (hero);
+			if (getDistance (this.gameObject, hero) > range) {
+				setState (State.State_back);
+			}
+			if (getDistance (this.gameObject, hero) < attackRange) {
+				setState (State.State_attack);
+			}
+			break;
+		case State.State_attack:
+			playAnimation (attackAnimationName);
+			if (getDistance (this.gameObject, hero) > attackRange) {
+				setState (State.State_follow);
+			}
+			break;
+		case State.State_back:
+			playAnimation (moveAnimation);
+			moveTo (birthPoint);
+			if (getDistance (this.gameObject, birthPoint) < 0.2) {
+				setState (State.State_idle);
+			}
+			break;
+		case State.State_patrol:
+			break;
+		}
+	}
+
+	//计算2个对象之间距离
+	float getDistance(GameObject a, GameObject b)
+	{
+		return Mathf.Abs(b.transform.localPosition.x - a.transform.localPosition.x);
+	}
+
+	//移动至对象B
+	void moveTo(GameObject b)
+	{
+		float direction = b.transform.localPosition.x - gameObject.transform.localPosition.x;
+		transform.localScale = new Vector3(direction > 0 ? 1.0f : -1.0f, transform.localScale.y, transform.localScale.z);
+		transform.Translate (speed * (direction > 0 ? 1.0f : -1.0f) / 60.0f, 0, 0);
+	}
+
+	//播放动画
+	void playAnimation(string animation)
+	{
+		if (curAnimationName != animation) {
+			Debug.Log ("player animation" + animation);
+			spineAnimationState.SetAnimation (0, animation, true);
+			curAnimationName = animation;
+		}
+	}
+
+	void setState(State state)
+	{
+		this.state = state;
+		Debug.Log ("new state" + state);
+	}
+}
---
+++ Assets/AI_FSM2.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+using Spine.Unity;
+
+public class AI_FSM2 : MonoBehaviour {
+
+	public GameObject hero;
+	public GameObject birthPoint;
+
+
+	enum State {
+		State_Idle, //待机
+		State_Follow, //跟随
+		State_Back, //返回出生点
+		State_Attack, //攻击状态
+	}
+
+	enum IdleState {
+		IdleState_stand, //站立不动
+		IdleState_play_sword, //舞剑
+		IdleState_play_sheild, //举盾
+	}
+
+	State state;
+	IdleState idleState;
+
+	string animation_idle1 = "Idle";
+	string animation_idle2 = "Idle2";
+	string animation_idle3 = "Idle3";
+	string animation_attack = "Attack";
+	string animation_move = "Move";
+
+	SkeletonAnimation skeletonAnimation;
+	string curAnimationName;
+	Spine.AnimationState spineAnimationState;
+
+	float curTime;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("AI 启动");
+		state = State.State_Idle;
+		idleState = IdleState.IdleState_stand;
+		curTime = Time.time;
+
+		skeletonAnimation = GetComponent<SkeletonAnimation>();
+		spineAnimationState = skeletonAnimation.state;
+
+		spineAnimationState.SetAnimation (0, animation_idle1, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("当前状态为：" + state);
+
+		switch (state) {
+
+		case State.State_Idle:
+			float newTime = Time.time;
+			Debug.Log ("当前子状态为：" + idleState);
+
+			switch (idleState) {
+			case IdleState.IdleState_stand:
+				if (newTime - curTime > 4) {
+					curTime = newTime;
+					idleState = IdleState.IdleState_play_sword;
+					spineAnimationState.SetAnimation (0, animation_idle2, true);
+				}
+				break;
+			case IdleState.IdleState_play_sword:
+				if (newTime - curTime > 4) {
+					curTime = newTime;
+					idleState = IdleState.IdleState_play_sheild;
+					spineAnimationState.SetAnimation (0, animation_idle3, true);
+				}
+				break;
+			case IdleState.IdleState_play_sheild:
+				if (newTime - curTime > 4) {
+					curTime = newTime;
+					idleState = IdleState.IdleState_stand;
+					spineAnimationState.SetAnimation (0, animation_idle1, true);
+				}
+				break;
+			}
+
+			if (getDistanceAbs (hero, this.gameObject) < 5.0f) {
+				state = State.State_Follow;
+
+				spineAnimationState.SetAnimation (0, animation_move, true);
+			}
+			break;
+
+		case State.State_Follow:
+			gotoObject (hero);
+
+			if (getDistanceAbs (hero, this.gameObject) > 5.0f) {
+				state = State.State_Back;
+			}
+			if (getDistanceAbs (hero, this.gameObject) < 1.0f) {
+				state = State.State_Attack;
+				spineAnimationState.SetAnimation (0, animation_attack, true);
+			}
+			break;
+
+		case State.State_Back:
+			gotoObject (birthPoint);
+
+			if (getDistanceAbs (this.gameObject, birthPoint) < 0.1f) {
+				state = State.State_Idle;
+				spineAnimationState.SetAnimation (0, animation_idle1, true);				
+			}
+			break;
+
+		case State.State_Attack:
+			if (getDistanceAbs (this.gameObject, hero) > 1.5f) {
+				state = State.State_Follow;
+				spineAnimationState.SetAnimation (0, animation_move, true);
+			}
+			break;
+		}
+	}
+
+	void gotoObject(GameObject target)
+	{
+		float direction = getDistance (this.gameObject, target) > 0 ? 1 : -1;
+		this.gameObject.transform.localScale = new Vector3 (-direction,
+			this.gameObject.transform.localScale.y, 
+			this.gameObject.transform.localScale.z);
+
+		this.gameObject.transform.Translate (new Vector3 (0.02f * -direction, 0, 0));
+	}
+
+	float getDistance(GameObject a, GameObject b)
+	{
+		return a.transform.localPosition.x - b.transform.localPosition.x;
+	}
+
+	float getDistanceAbs(GameObject a, GameObject b)
+	{
+		return Mathf.Abs(a.transform.localPosition.x - b.transform.localPosition.x);
+	}
+}
